c3e0ecca6630464c0cc260e472a7ce621c0f9fc4
Thomas Winkler <thomas.winkler@wunderbyte.at>
2019-10-21 13:54:37 +0200

    newpic

--- submission/signing/amd/src/signingjs.js
+++ submission/signing/amd/src/signingjs.js
@@ -123,13 +123,3 @@ function save() {
     }
     $('#signing_text').val("this.href");
 }
-
-function isCanvasBlank(canvas) {
-  const context = canvas.getContext('2d');
-
-  const pixelBuffer = new Uint32Array(
-    context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
-  );
-
-  return !pixelBuffer.some(color => color !== 0);
-}
