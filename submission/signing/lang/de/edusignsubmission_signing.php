a9baa82631129fb6bfbdbd8c1e1aee8c13f1dd9c
Thomas Winkler <thomas.winkler@wunderbyte.at>
2019-10-31 13:38:21 +0100

    langfiles

---
+++ submission/signing/lang/de/edusignsubmission_signing.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'edusignsubmission_signing', language 'en'
+ *
+ * @package   edusignsubmission_signing
+ * @copyright 2012 NetSpot {@link http://www.netspot.com.au}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allowsigningsubmissions'] = 'Aktiviert';
+$string['default'] = 'Standardmäßig aktiviert';
+$string['default_help'] = 'Die gewählte Methode für die Abgabe von Lösungen wird für alle neuen Aufgaben voreingestellt.';
+$string['enabled'] = 'Unterschrift';
+$string['enabled_help'] = 'Diese Methode ermöglicht den Teilnehmer/innen die Lösung direkt in einem Texteingabefeld mit Editor einzutragen.';
+$string['eventassessableuploaded'] = 'Eine Unterschrift wurde hochgeladen';
+$string['nosubmission'] = 'Unterschrift fehlt';
+$string['signing'] = 'Unterschrift';
+$string['signingfilename'] = 'signing.html';
+$string['signingsubmission'] = 'Erlaube Unterschriftabgabe';
+$string['numwords'] = '({$a} words)';
+$string['numwordsforlog'] = 'Submission word count: {$a} words';
+$string['pluginname'] = 'Unterschrift';
+$string['privacy:metadata:edusignmentid'] = 'eduSign ID';
+$string['privacy:metadata:filepurpose'] = 'Files that are embedded in the text submission.';
+$string['privacy:metadata:submissionpurpose'] = 'The submission ID that links to submissions for the user.';
+$string['privacy:metadata:tablepurpose'] = 'Stores the text submission for each attempt.';
+$string['privacy:metadata:textpurpose'] = 'The actual text submitted for this attempt of the edusignment.';
+$string['privacy:path'] = 'Submission Text';
+$string['wordlimit'] = 'Word limit';
+$string['wordlimit_help'] = 'If online text submissions are enabled, this is the maximum number ' .
+        'of words that each student will be allowed to submit.';
+$string['wordlimitexceeded'] = 'The word limit for this edusignment is {$a->limit} words and you ' .
+        'are attempting to submit {$a->count} words. Please review your submission and try again.';
+
